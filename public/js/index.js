720cea26ce881fda78d25797f9e553a9b9b8d755
mahmoudalwadia <mahmoud.alwadia@gmail.com>
2017-08-23 18:09:14 +0300

    aaaaah

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ var login = document.getElementById('login');
 var signUp = document.getElementById('signUp');
 var loginForm = document.getElementById('loginForm');
 var signUpForm = document.getElementById('signUpForm');
-var TopicsDiv = document.getElementById('Topics');
+// var TopicsDiv = document.getElementById('Topics');
 
 login.addEventListener('click', () => {
   loginForm.style.display = 'block';
@@ -14,13 +14,9 @@ signUp.addEventListener('click', () => {
   loginForm.style.display = 'none';
 });
 
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    get('/viewTopic', (response) => { 
+document.addEventListener('DOMContentLoaded', function () {
+  get('/viewTopic', (response) => {// eslint-disable-line
         // Topics.innerHTML = generateOptions(response);
-        console.log(response)
-    })
+    console.log(response);
+  });
 }, false);
